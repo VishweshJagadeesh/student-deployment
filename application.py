52684d081a56bccb23cd0fa44329b63251187650
vishwesh jagadeesh <vishweshjagadeesh@gmail.com>
2025-06-07 18:36:52 +0530

    application renamed

--- application.py
+++ application.py
@@ -17,7 +17,7 @@ app=application
 # def index():
 #     return render_template('index.html')
 
-@app.route('/', methods=['GET','POST'])
+@application.route('/', methods=['GET','POST'])
 def predict_datapoint():
     if request.method=="GET":
         return render_template('home.html')
@@ -43,4 +43,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    application.run(host="0.0.0.0",debug=True)
